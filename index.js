3c53f95b0ecd09d501642059bbb3173428197821
SmartKitchen <zubitex40@gmail.com>
2019-09-26 10:54:59 +0100

    second commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express=require('express')
+const path=require('path')
+const ejs=require('ejs');
+const morgan=require('morgan')
+const PORT = process.env.PORT || 5000
+
+var app=new express()
+
+app
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+
+  app.get('/', (req, res) => res.render('pages/index'))
+
+  app.get('/welcome',(req, res) => {
+      return res.render('pages/welcome');
+  })
+
+  app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
